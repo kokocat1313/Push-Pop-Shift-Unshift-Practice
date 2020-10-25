c1982628e0351d0eceec0eac491ed3fe9397888a
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2020-10-24 21:32:25 -0700

    1st commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINEplane
+planets.shift;
+planets.push('Saturn');
+planets.unshift('Mercury');
+
+console.log(planets)
